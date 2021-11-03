54e56b52f4cadad42289cff932a585dc3040a19c
felipe.piacsek <felipe.piacsek@gmail.com>
2021-11-02 23:42:47 -0300

    [DB] Adds seeds for all entities

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'ffaker'
+
+# Tenants
+10.times do
+  Tenant.create(name: FFaker::Company.name, api_key: FFaker::Lorem.characters(64))
+end
+
+# Users
+users = []
+20.times do
+  users << User.create(name: FFaker::Name.name)
+end
+
+# Questions and Answers
+20.times do
+  question = Question.create(title: FFaker::HipsterIpsum.sentence.gsub(/\.$/, "?"),
+                             is_private: FFaker::Boolean.random, asker: users.sample)
+  (1 + rand(3)).times do
+    question.answers.create(body: FFaker::HipsterIpsum.sentence, provider: users.sample)
+  end
+end
