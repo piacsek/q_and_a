e4929917a18ed40ba25023760573c94d77207152
felipe.piacsek <felipe.piacsek@gmail.com>
2021-11-06 17:41:35 -0300

    [API] Adds authentication layer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  def authenticate
+    api_key = request.headers['Authorization']
+    tenant = Tenant.select(:id).find_by(api_key: api_key)
+
+    if tenant.blank?
+      unauthorized!
+    else
+      count_utilization tenant
+    end
+  end
+
+  def unauthorized!
+    render json: { message: 'Invalid authorization header' }, status: :unauthorized
+  end
+
+  def count_utilization(tenant)
+    # TODO
+  end
+
+
 end
--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -1,27 +1,64 @@
 require './test/test_helper'
 
 class QuestionsControllerTest < ActionDispatch::IntegrationTest
-  test "returns HTTP 200 and JSON response containing all public questions" do
-    create_list :question, 5, :with_answers
 
-    expected_response = Question.is_public.includes(:asker, answers: :provider).map do |question|
-      QuestionSerializer.new(question)
-    end.to_json
+  class AuthenticatedTest < ActionDispatch::IntegrationTest
 
-    get api_questions_url
+    test "returns HTTP 200 and JSON response containing all public questions for" do
+      create_list :question, 5, :with_answers
+      create :tenant
 
-    assert_response :success
-    assert expected_response == response.body, "\nExpected:\n#{expected_response} \n\nActual:\n#{response.body.to_json}"
+      expected_response = Question.is_public.includes(:asker, answers: :provider).map do |question|
+        QuestionSerializer.new(question)
+      end.to_json
+      api_key = Tenant.first.api_key
+
+      get api_questions_url, headers: { 'Authorization' => api_key }
+
+      assert_response :success
+      assert expected_response == response.body, "\nExpected:\n#{expected_response} \n\nActual:\n#{response.body.to_json}"
+    end
+
+    test 'returns HTTP 200 and a JSON response with an empty list if there are only private questions' do
+      create_list :question, 3, :private, :with_answers
+      create :tenant
+
+      expected_response = [].to_json
+      api_key = Tenant.first.api_key
+
+      get api_questions_url, headers: { 'Authorization' => api_key }
+
+      assert_response :success
+      assert expected_response == response.body, "\nExpected:\n#{expected_response} \n\nActual:\n#{response.body.to_json}"
+    end
+
+    test 'counts api_key utilization' do
+      skip
+      create :tenant
+
+    end
   end
 
-  test 'returns HTTP 200 and a JSON response with an empty list if there are only private questions' do
-    create_list :question, 3, :private, :with_answers
+  class AuthenticationFailureTest < ActionDispatch::IntegrationTest
 
-    expected_response = [].to_json
+    test 'returns http 401 when missing api_key' do
+      get api_questions_url
 
-    get api_questions_url
+      assert_response :unauthorized
+    end
 
-    assert_response :success
-    assert expected_response == response.body, "\nExpected:\n#{expected_response} \n\nActual:\n#{response.body.to_json}"
+    test 'returns http 401 when provided api_key is invalid' do
+      get api_questions_url, headers: { 'Authorization' => 'Non-existent' }
+
+      assert_response :unauthorized
+    end
+
+    test 'returns http 401 & does not break when provided api_key is too long' do
+      long_api_key = FFaker::Lorem.characters(16384)
+
+      get api_questions_url, headers: { 'Authorization' => long_api_key }
+
+      assert_response :unauthorized
+    end
   end
 end
