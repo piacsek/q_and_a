9e24d6b03e50655a0dbf3ab24e6fec2bab0c330f
Jonathan Stiansen <jonathan.stiansen@gmail.com>
2021-11-08 15:38:10 -0700

    chore: small change to not give the impression that passing in name makes a difference

--- test/factories/answers.rb
+++ test/factories/answers.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :answer do
     body { FFaker::HipsterIpsum.sentence }
     question
-    provider { association :user, name: FFaker::Name.name }
+    provider { create(:user) }
   end
 end
