22a9c532e146c47d29eb28d8b547cb2430d99dca
Jonathan Stiansen <jonathan.stiansen@gmail.com>
2021-11-08 15:50:37 -0700

    chore: add database cleaner

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
+require "database_cleaner-active_record"
+DatabaseCleaner[:active_record].strategy = :transaction
+
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -8,6 +11,12 @@ class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  def setup
+    DatabaseCleaner.start
+  end
 
+  def teardown
+    DatabaseCleaner.clean
+  end
   # Add more helper methods to be used by all tests here...
 end
